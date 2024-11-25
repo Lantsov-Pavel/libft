d7f9365e45552330c2daf0013384d3816531ca89
plantsov <plantsov@student.42berlin.de>
2024-11-25 16:18:33 +0100

    add_memmove

---
+++ ft_memmove.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+    char *d;
+    char *s;
+    size_t i;
+
+    d = (char *)dest;
+    s = (char *)src;
+    if (d == s || n == 0)
+        return dest;
+
+    if (d < s || d >= (s + n))
+    {
+        i = 0;
+        while (i < n)
+        {
+            d[i] = s[i];
+            i++;
+        }
+    }else{
+        i = n - 1;
+         while (i > 0)
+        {           
+            d[i] = s[i];
+            i--;
+        }
+    }
+    return dest;
+}
